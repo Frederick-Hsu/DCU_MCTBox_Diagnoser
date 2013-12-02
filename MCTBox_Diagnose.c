6911dbf5f8ce280d65be2b488ef285f40584c8b0
XU ZAN <zan.xu@hella.com>
2013-12-02 17:24:21 +0800

    Insert the head comment for project description and source file description.

--- MCTBox_Diagnose.c
+++ MCTBox_Diagnose.c
@@ -1,3 +1,18 @@
+/**************************************************************************************************************
+ * Project		: MCTBox_Diagnose
+ * Description	: This project will build a graphic user utility to access the MCTBox instrument through
+ *				  MCTBox_APIs. It provides the diagnosis interface for testing user, and can be integrated
+ *				  into the "Test Sequenzer" application.
+ * ------------------------------------------------------------------------------------------------------------
+ * File name	: MCTBox_Diagnose.c
+ * Description	: This file implements the main() entry function.
+ * Creator		: XU ZAN	[HSE-OT]
+ * Creation date: Mon.	Dec. 02, 2013
+ * Copyright(C)		2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ **************************************************************************************************************/
+
 #include <cvirte.h>		
 #include <userint.h>
 #include "MCTBox_Diagnose.h"
