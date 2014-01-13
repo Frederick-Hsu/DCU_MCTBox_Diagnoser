d8d502edbbea9ec00f0da858847427d65a7fb23e
XU ZAN <zan.xu@hella.com>
2014-01-13 14:35:48 +0800

    設計Switch Control Panel的用戶界面.

---
+++ Catalogue_SwitchControl/MCTBox_Diagnose_SwitchControlPanel.h
@@ -0,0 +1,140 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2014. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  pnlSwitch                        1       /* callback function: CB_ExitSwitchControlPanel */
+#define  pnlSwitch_cmdbtnClosePanel       2       /* control type: command, callback function: CB_CloseSwitchCtrlPanel */
+#define  pnlSwitch_binswBoardIDBit7       3       /* control type: binary, callback function: CB_binswBoardIDBit7_OnChange */
+#define  pnlSwitch_binswBoardIDBit6       4       /* control type: binary, callback function: CB_binswBoardIDBit6_OnChange */
+#define  pnlSwitch_binswBoardIDBit5       5       /* control type: binary, callback function: CB_binswBoardIDBit5_OnChange */
+#define  pnlSwitch_binswBoardIDBit4       6       /* control type: binary, callback function: CB_binswBoardIDBit4_OnChange */
+#define  pnlSwitch_binswBoardIDBit3       7       /* control type: binary, callback function: CB_binswBoardIDBit3_OnChange */
+#define  pnlSwitch_binswBoardIDBit2       8       /* control type: binary, callback function: CB_binswBoardIDBit2_OnChange */
+#define  pnlSwitch_binswBoardIDBit1       9       /* control type: binary, callback function: CB_binswBoardIDBit1_OnChange */
+#define  pnlSwitch_binswBoardIDBit0       10      /* control type: binary, callback function: CB_binswBoardIDBit0_OnChange */
+#define  pnlSwitch_DECORATION             11      /* control type: deco, callback function: (none) */
+#define  pnlSwitch_txtlblBoardID          12      /* control type: textMsg, callback function: (none) */
+#define  pnlSwitch_binSwitch24            13      /* control type: binary, callback function: CB_binSwitch24_OnClick */
+#define  pnlSwitch_binSwitch23            14      /* control type: binary, callback function: CB_binSwitch23_OnClick */
+#define  pnlSwitch_binSwitch22            15      /* control type: binary, callback function: CB_binSwitch22_OnClick */
+#define  pnlSwitch_binSwitch21            16      /* control type: binary, callback function: CB_binSwitch21_OnClick */
+#define  pnlSwitch_binSwitch20            17      /* control type: binary, callback function: CB_binSwitch20_OnClick */
+#define  pnlSwitch_ledSwitch24            18      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch23            19      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch22            20      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch21            21      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_binSwitch19            22      /* control type: binary, callback function: CB_binSwitch19_OnClick */
+#define  pnlSwitch_binSwitch18            23      /* control type: binary, callback function: CB_binSwitch18_OnClick */
+#define  pnlSwitch_binSwitch17            24      /* control type: binary, callback function: CB_binSwitch17_OnClick */
+#define  pnlSwitch_binSwitch16            25      /* control type: binary, callback function: CB_binSwitch16_OnClick */
+#define  pnlSwitch_ledSwitch20            26      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch19            27      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch18            28      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch17            29      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_binSwitch15            30      /* control type: binary, callback function: CB_binSwitch15_OnClick */
+#define  pnlSwitch_SPLITTER_8             31      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_binSwitch14            32      /* control type: binary, callback function: CB_binSwitch14_OnClick */
+#define  pnlSwitch_binSwitch13            33      /* control type: binary, callback function: CB_binSwitch13_OnClick */
+#define  pnlSwitch_binSwitch12            34      /* control type: binary, callback function: CB_binSwitch12_OnClick */
+#define  pnlSwitch_ledSwitch16            35      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch15            36      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch14            37      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch13            38      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_binSwitch11            39      /* control type: binary, callback function: CB_binSwitch11_OnClick */
+#define  pnlSwitch_SPLITTER_7             40      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_binSwitch10            41      /* control type: binary, callback function: CB_binSwitch10_OnClick */
+#define  pnlSwitch_binSwitch9             42      /* control type: binary, callback function: CB_binSwitch9_OnClick */
+#define  pnlSwitch_binSwitch8             43      /* control type: binary, callback function: CB_binSwitch8_OnClick */
+#define  pnlSwitch_ledSwitch12            44      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch11            45      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch10            46      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch9             47      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_binSwitch7             48      /* control type: binary, callback function: CB_binSwitch7_OnClick */
+#define  pnlSwitch_SPLITTER_6             49      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_binSwitch6             50      /* control type: binary, callback function: CB_binSwitch6_OnClick */
+#define  pnlSwitch_binSwitch5             51      /* control type: binary, callback function: CB_binSwitch5_OnClick */
+#define  pnlSwitch_binSwitch4             52      /* control type: binary, callback function: CB_binSwitch4_OnClick */
+#define  pnlSwitch_ledSwitch8             53      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch7             54      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch6             55      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch5             56      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_binSwitch3             57      /* control type: binary, callback function: CB_binSwitch3_OnClick */
+#define  pnlSwitch_SPLITTER_5             58      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_binSwitch2             59      /* control type: binary, callback function: CB_binSwitch2_OnClick */
+#define  pnlSwitch_binSwitch1             60      /* control type: binary, callback function: CB_binSwitch1_OnClick */
+#define  pnlSwitch_txtBoardID             61      /* control type: textMsg, callback function: (none) */
+#define  pnlSwitch_ledSwitch4             62      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch3             63      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch2             64      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_ledSwitch1             65      /* control type: LED, callback function: (none) */
+#define  pnlSwitch_txtlblSwitchMatrix     66      /* control type: textMsg, callback function: (none) */
+#define  pnlSwitch_SPLITTER               67      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_SPLITTER_10            68      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_SPLITTER_9             69      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_SPLITTER_2             70      /* control type: splitter, callback function: (none) */
+#define  pnlSwitch_DECORATION_2           71      /* control type: deco, callback function: (none) */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */
+
+int  CVICALLBACK CB_binswBoardIDBit0_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit1_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit2_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit3_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit4_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit5_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit6_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binswBoardIDBit7_OnChange(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch10_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch11_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch12_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch13_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch14_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch15_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch16_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch17_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch18_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch19_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch1_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch20_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch21_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch22_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch23_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch24_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch2_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch3_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch4_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch5_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch6_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch7_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch8_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_binSwitch9_OnClick(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_CloseSwitchCtrlPanel(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK CB_ExitSwitchControlPanel(int panel, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
