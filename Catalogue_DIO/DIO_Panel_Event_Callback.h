c37f9acd7398e8c2ad8570e10be4e16272166229
XU ZAN <zan.xu@hella.com>
2014-02-10 16:24:12 +0800

    Change the old header file "Dout_Panel_Event_Callback.h"  to new header file "DIO_Panel_Event_Callback.h"

--- Catalogue_DIO/DIO_Panel_Event_Callback.h
+++ Catalogue_DIO/DIO_Panel_Event_Callback.h
@@ -1,5 +1,5 @@
 /*********************************************************************************************************************************
- * File name		: Dout_Panel_Event_Callback.h
+ * File name		: DIO_Panel_Event_Callback.h
  * Description		: Declare and implement the event call-back functions defined on the Dout panel.
  * Creator			: XU ZAN
  * Creation date	: Mon.	Jan. 13, 2014
@@ -8,8 +8,8 @@
  *
  *********************************************************************************************************************************/
 
-#ifndef DOUT_PANEL_EVENT_CALLBACK_H
-#define DOUT_PANEL_EVENT_CALLBACK_H
+#ifndef DIO_PANEL_EVENT_CALLBACK_H
+#define DIO_PANEL_EVENT_CALLBACK_H
 
 	#ifdef __cplusplus
 	    extern "C" 
@@ -32,9 +32,20 @@
 			// Global functions
 			int ChangeDoutBoardID(int hPanel, int iEvent);
 			int ControlSingleDoutPort(int hPanel, int iEvent, int iDoutPortCtrlID, int iDoutPortNr);
+			int ChangeDinBoardID(int hPanel, int iEvent);
+			
+			int CVICALLBACK CB_AsyncTimerContinuousQueryDinPortState(int iReserved,
+																	 int iAsyncTimerID,
+																	 int event,
+																	 void *callbackData,
+																	 int eventData1,
+																	 int eventData2);
+			
+			void TurnOnOffCurrentLED_ByChannelNr(int hPanel, int iChannelNr, int iState);
+			void TurnOnOffCurrentLED(int hPanel, int iLedCtrlID, int iState);
 
 	#ifdef __cplusplus
 	    }
 	#endif
 
-#endif  /* ndef DOUT_PANEL_EVENT_CALLBACK_H */
+#endif  /* ndef DIO_PANEL_EVENT_CALLBACK_H */
